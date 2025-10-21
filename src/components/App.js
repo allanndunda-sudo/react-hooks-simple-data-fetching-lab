756601504dc3782e7659c8a79e7cb424c6c2b64e
allanndunda <allanndunda>
2025-10-21 20:43:35 +0300

    Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching dog image:", error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={dogImage} alt="A Random Dog" />;
+}
+
+export default App;
